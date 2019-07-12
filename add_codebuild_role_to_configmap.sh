ec9a9b69e1f0682491b19c4d1bcfc23cd6190df1
Pedro Mendoza <pedrojmendoza@gmail.com>
2019-07-12 11:39:49 +0200

    initial version - code version selectable, config version fixed to last

---
+++ add_codebuild_role_to_configmap.sh
@@ -0,0 +1,5 @@
+ROLE="    - rolearn: arn:aws:iam::$ACCOUNT_ID:role/CodeBuildEksRole\n      username: build\n      groups:\n        - system:masters"
+
+kubectl get -n kube-system configmap/aws-auth -o yaml | awk "/mapRoles: \|/{print;print \"$ROLE\";next}1" > /tmp/aws-auth-patch.yml
+
+kubectl patch configmap/aws-auth -n kube-system --patch "$(cat /tmp/aws-auth-patch.yml)"
